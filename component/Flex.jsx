1abfa3af3d884bbca778bf4aff875e2f60714792
OmarMahin <okmahin2@gmail.com>
2024-06-18 09:57:54 +0600

    Login and signup again

--- component/Flex.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Flex = ({children, className, onClick}) => {
-  return (
-    <div className={`lg:flex ${className}`} onClick = {onClick}>{children}</div>
-  )
-}
-
-export default Flex
