e228e5db66e6ef58631313af28c01fee188c9e22
OmarMahin <okmahin2@gmail.com>
2024-08-06 22:44:42 +0600

    Banner Image optimization

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -11,6 +11,11 @@ const Banner = () => {
 	let normal_banner = "Banner.png"
 	let banner_for_phone = "Banner_For_Phone.png"
 
+	const mobile_img_set =
+		"images/Banner/smaller_devices/Banner_For_Phone-200.png 200w, images/Banner/smaller_devices/Banner_For_Phone-400.png 400w, images/Banner/smaller_devices/Banner_For_Phone-600.png 600w, images/Banner/smaller_devices/Banner_For_Phone-768.png 768w"
+
+	const desktop_img_set = "images/Banner/bigger_device/Banner-1000.png 1000w, images/Banner/bigger_device/Banner-1200.png 1200w"
+
 	useEffect(() => {
 		function checkWindowSize() {
 			if (window.innerWidth < 768) {
@@ -27,9 +32,12 @@ const Banner = () => {
 			<AnimatePage>
 				<Container>
 					<Image
-						src={`/images/${smallerDisplay ? banner_for_phone : normal_banner}`}
-						className='h-full'
+						imrSrcSet={smallerDisplay ? mobile_img_set : desktop_img_set}
+						src={smallerDisplay ? `images/Banner/smaller_devices/Banner_For_Phone-768.png` : `images/Banner/bigger_device/Banner-1200.png`}
+						className='w-full'
 						alt={"Banner Image"}
+						sizes={'(max-width: 200px) 200px, ( max-width: 400px) 400px, (max-width: 600px) 600px, (max-width: 768px) 768px, ( max-width: 1000px) 1000px, (max-width: 1200px) 1200px'}
+						fetchpriority={"high"}
 					></Image>
 				</Container>
 			</AnimatePage>
--- src/component/Image.jsx
+++ src/component/Image.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Image = ({src, alt, className, onLoad}) => {
+const Image = ({src, alt, className, onLoad, fetchpriority = '', sizes = '', imrSrcSet = '', loadingType = ''}) => {
   return (
-      <img className={`${className}`} src = {src} alt = {alt} onLoad = {onLoad}></img>
+      <img className={`${className}`} srcSet={imrSrcSet} src = {src} alt = {alt} onLoad = {onLoad} fetchPriority={fetchpriority} sizes={sizes} loading={loadingType} ></img>
   )
 }
 
